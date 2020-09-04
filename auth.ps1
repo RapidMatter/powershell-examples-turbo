d7d37933ed71187cbc0abd4e5469166559724ad4
Eric Wright <eric.wright@turbonomic.com>
2020-09-04 15:08:32 -0400

    working auth for vCenter

---
+++ auth.ps1
@@ -0,0 +1,21 @@
+
+$vcenter_url = 'https://vcenter.demo.turbonomic.com/rest/com/vmware/cis/session'
+
+$Credential = Get-Credential
+$vcauth = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($Credential.UserName+':'+$Credential.GetNetworkCredential().Password))
+$head = @{
+  'Authorization' = "Basic $vcauth"
+}
+
+$tokenresult = Invoke-WebRequest -Uri $vcenter_url -Method Post -Headers $head -SkipCertificateCheck
+$token = (ConvertFrom-Json $tokenresult.Content).value
+$session = @{'vmware-api-session-id' = $token}
+
+Write-host $tokenresult	
+Write-host $token
+
+$session = @{'vmware-api-session-id' = $token}
+
+$vmresults = Invoke-WebRequest -Uri https://vcenter.demo.turbonomic.com/rest/vcenter/vm -SkipCertificateCheck -Headers $session
+
+$vmresults
