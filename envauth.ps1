e792937ebc89a28b9581c7456ed085cf3e3cc735
Eric Wright <eric.wright@turbonomic.com>
2020-09-09 16:33:21 -0400

    adding comments to envauth

--- envauth.ps1
+++ envauth.ps1
@@ -1,24 +1,42 @@
+# Set your vCenter server
+$vcenter = (Get-ChildItem Env:VCSERVER).value
 
+# Define the session URL for vCenter
 $vcenter_url = 'https://vcenter.demo.turbonomic.com/rest/com/vmware/cis/session'
 
+# Set your vCenter user from the VCUSER environment variable
 $User = (Get-ChildItem Env:VCUSER).value
+
+# Set your vCenter password from teh VCPASS environment variable
 $PWord = ConvertTo-SecureString -String (Get-ChildItem Env:VCPASS).value -AsPlainText -Force
+
+# Set the credential object 
 $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
 
+# Create HTTP header to authenticate to vCenter
 $vcauth = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($Credential.UserName+':'+$Credential.GetNetworkCredential().Password))
 $head = @{
   'Authorization' = "Basic $vcauth"
 }
 
+# Get authentication token as JSON object from vCenter 
 $tokenresult = Invoke-WebRequest -Uri $vcenter_url -Method Post -Headers $head -SkipCertificateCheck
+
+# Get the value from the token JSON result
 $token = (ConvertFrom-Json $tokenresult.Content).value
+
+# Set the session header value using the new token
 $session = @{'vmware-api-session-id' = $token}
 
-Write-host $tokenresult	
-Write-host $token
+# Uncomment these two Write-Host values to write the results to console
+# Write-host $tokenresult	
+# Write-host $token
 
-$session = @{'vmware-api-session-id' = $token}
+# THIS IS DUPLICATE
+#$session = @{'vmware-api-session-id' = $token}
 
+# Get list of VMs as a JSON result
 $vmresults = Invoke-WebRequest -Uri https://vcenter.demo.turbonomic.com/rest/vcenter/vm -SkipCertificateCheck -Headers $session
 
+# Output JSON result to console 
 $vmresults
